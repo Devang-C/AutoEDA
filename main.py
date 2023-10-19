7871e274226cccfc937dc6ba5c11c34a57937c72
Devang-C <devangchavan0204@gmail.com>
2023-10-19 20:14:49 +0530

    created seperate section for data preprocessing

--- main.py
+++ main.py
@@ -133,153 +133,163 @@ if uploaded_file:
 # Display the dataset preview or any other content here
 if uploaded_file is None:
     # st.subheader("Welcome to DataExplora!")
-    st.write("Use the sidebar to upload a CSV file and explore your data.")
+    st.markdown("#### Use the sidebar to upload a CSV file and explore your data.")
 else:
 
-    tab1, tab2, tab3 = st.tabs(['📊 Dataset Overview :clipboard', "🔎 Data Exploration and Visualization","🛠️ Data Preprocessing"])
-    num_columns, cat_columns = function.categorical_numerical(df)
+    data_exploration_button = st.sidebar.button("**Data Exploration**")
+    data_preprocessing_button = st.sidebar.button("**Data Preprocessing**")
     
-    
-    with tab1: # DATASET OVERVIEW TAB
+    if not (data_exploration_button or data_preprocessing_button):
+        st.write("#### Use the sidebar to navigate to either Data Exploration or Data Preprocessing")
 
-        function.display_dataset_overview(df,cat_columns,num_columns)
+    if data_exploration_button:
         
-        function.display_missing_values(df)
+        tab1, tab2 = st.tabs(['📊 Dataset Overview :clipboard', "🔎 Data Exploration and Visualization"])
+        num_columns, cat_columns = function.categorical_numerical(df)
         
-        function.display_statistics_visualization(df,cat_columns,num_columns)
-
-        function.display_data_types(df)
-
-        function.search_column(df)
-
-    with tab2: 
-        df_description = df.describe()
-        st.subheader("Analyze Individual Feature Distribution")
-        st.markdown("Here, you can explore individual numerical features, visualize their distributions, and analyze relationships between features.")
-
-
-        if len(num_columns)!=0:
-            st.write("#### Understanding Numerical Features")
-            feature = st.selectbox(label="Select Numerical Feature", options=num_columns, index=0)
-
-            # Display summary statistics
-            null_count = df[feature].isnull().sum()
-            st.write("Count: ", df_description[feature]['count'])
-            st.write("Missing Count: ", null_count)
-            st.write("Mean: ", df_description[feature]['mean'])
-            st.write("Standard Deviation: ", df_description[feature]['std'])
-            st.write("Minimum: ", df_description[feature]['min'])
-            st.write("Maximum: ", df_description[feature]['max'])
-
-            # Create distribution plots
-            st.subheader("Distribution Plots")
-            plot_type = st.selectbox(label="Select Plot Type", options=["Histogram", "Scatter Plot","Density Plot", "Box Plot"])
-
-            if plot_type == "Histogram":
-                fig = px.histogram(df, x=feature, title=f'Histogram of {feature}')
-
-            elif plot_type=="Scatter Plot":
-                fig = px.scatter(df,x=feature,y=feature,title=f"Scatter plot of {feature}")
-            elif plot_type == "Density Plot":
-                fig = px.density_contour(df, x=feature, title=f'Density Plot of {feature}')
-            else:
-                fig = px.box(df, y=feature, title=f'Box Plot of {feature}')
-
-            st.plotly_chart(fig, use_container_width=True)
-
-            # Create scatter plot
-            st.subheader("Scatter Plot")
-            x_feature = st.selectbox(label="Select X-Axis Feature", options=num_columns, index=0)
-            y_feature = st.selectbox(label="Select Y-Axis Feature", options=num_columns, index=1)
-
-            scatter_fig = px.scatter(df, x=x_feature, y=y_feature, title=f'Scatter Plot: {x_feature} vs {y_feature}')
-            st.plotly_chart(scatter_fig, use_container_width=True)
-
-        else:
-            st.info("The dataset does not have any numerical columns")
-
-
-        if len(cat_columns)!=0:
-
-            st.subheader("Categorical Variable Analysis")
-            categorical_feature = st.selectbox(label="Select Categorical Feature",options=cat_columns)
-
-            categorical_plot_type = st.selectbox(label="Select Plot Type",options=["Bar Chart","Pie Chart","Stacked Bar Chart","Frequency Count"])
-
-            if categorical_plot_type =="Bar Chart":
-                fig = px.bar(df,x=categorical_feature,title=f"Bar Chart of {categorical_feature}")
-
-            elif categorical_plot_type == "Pie Chart":
-                fig = px.pie(df,names=categorical_feature,title=f"Pie Chart of {categorical_feature}")
-
-            elif categorical_plot_type == "Stacked Bar Chart":
-                st.write("Select a second categorical feature for stacking")
-                second_categorical_feature = st.selectbox(label="Select Second Categorical Feature",options=cat_columns)
-
-                fig = px.bar(df,x=categorical_feature,color=second_categorical_feature,title=f"Stacked Bar Chart of {categorical_feature} by {second_categorical_feature}")
-
-            elif categorical_plot_type == "Frequency Count":
-                cat_value_counts = df[categorical_feature].value_counts()
-                st.write(f"Frequency Count for {categorical_feature}: ")
-                st.write(cat_value_counts)
-
-            if categorical_plot_type!= "Frequency Count" and fig is not None:
-                st.plotly_chart(fig,use_container_width=True)   
-
-
-        else:
-            st.info("The dataset does not have any categorical columns")
-
-
-        st.subheader("Feature Exploration of Numerical Variables")
-        if len(num_columns)!=0:
-            selected_features = st.multiselect("Select Features for Exploration:", num_columns, default=num_columns[:2], key="feature_exploration")
-
-            if len(selected_features) < 2:
-                st.warning("Please select at least two numerical features for exploration.")
-            else:
-                st.subheader("Explore Relationships Between Features")
-
-                # Scatter Plot Matrix
-                if st.button("Generate Scatter Plot Matrix"):
-                    scatter_matrix_fig = px.scatter_matrix(df, dimensions=selected_features, title="Scatter Plot Matrix")
-                    st.plotly_chart(scatter_matrix_fig, use_container_width=True)
-
-                # Pair Plot
-                if st.button("Generate Pair Plot"):
-                    pair_plot_fig = sns.pairplot(df[selected_features])
-                    st.pyplot(pair_plot_fig)
-
-                # Correlation Heatmap
-                if st.button("Generate Correlation Heatmap"):
-                    correlation_matrix = df[selected_features].corr()
-                    plt.figure(figsize=(10, 6))
-                    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
-                    plt.title("Correlation Heatmap")
-                    st.pyplot(plt)     
-
-        else:
-            st.warning("The dataset does not contain any numerical variables")
-
-        # Create a bar graph to get relationship between categorical variable and numerical variable
-        st.subheader("Categorical and Numerical Variable Analysis")
-        if len(num_columns)!=0 and len(cat_columns)!=0:
-            categorical_feature_1 = st.selectbox(label="Categorical Feature", options=cat_columns)
-
         
-            numerical_feature_1 = st.selectbox(label="Numerical Feature", options=num_columns)
+        with tab1: # DATASET OVERVIEW TAB
 
-            # Group by the selected categorical column and calculate the mean of the numerical column
-            group_data = df.groupby(categorical_feature_1)[numerical_feature_1].mean().reset_index()
-
-            st.subheader("Relationship between Categorical and Numerical Variables")
-            st.write(f"Mean {numerical_feature_1} by {categorical_feature_1}")
+            function.display_dataset_overview(df,cat_columns,num_columns)
             
-            # Create a bar chart
-            fig = px.bar(group_data, x=categorical_feature_1, y=numerical_feature_1, title=f"{numerical_feature_1} by {categorical_feature_1}")
-            st.plotly_chart(fig, use_container_width=True)
+            function.display_missing_values(df)
+            
+            function.display_statistics_visualization(df,cat_columns,num_columns)
 
-        else:
-            st.warning("The dataset does not have any numerical variables. Hence Cannot Perform Categorical and Numerical Variable Analysis")
-        
+            function.display_data_types(df)
 
+            function.search_column(df)
+
+        with tab2: 
+            df_description = df.describe()
+            st.subheader("Analyze Individual Feature Distribution")
+            st.markdown("Here, you can explore individual numerical features, visualize their distributions, and analyze relationships between features.")
+
+
+            if len(num_columns)!=0:
+                st.write("#### Understanding Numerical Features")
+                feature = st.selectbox(label="Select Numerical Feature", options=num_columns, index=0)
+
+                # Display summary statistics
+                null_count = df[feature].isnull().sum()
+                st.write("Count: ", df_description[feature]['count'])
+                st.write("Missing Count: ", null_count)
+                st.write("Mean: ", df_description[feature]['mean'])
+                st.write("Standard Deviation: ", df_description[feature]['std'])
+                st.write("Minimum: ", df_description[feature]['min'])
+                st.write("Maximum: ", df_description[feature]['max'])
+
+                # Create distribution plots
+                st.subheader("Distribution Plots")
+                plot_type = st.selectbox(label="Select Plot Type", options=["Histogram", "Scatter Plot","Density Plot", "Box Plot"])
+
+                if plot_type == "Histogram":
+                    fig = px.histogram(df, x=feature, title=f'Histogram of {feature}')
+
+                elif plot_type=="Scatter Plot":
+                    fig = px.scatter(df,x=feature,y=feature,title=f"Scatter plot of {feature}")
+                elif plot_type == "Density Plot":
+                    fig = px.density_contour(df, x=feature, title=f'Density Plot of {feature}')
+                else:
+                    fig = px.box(df, y=feature, title=f'Box Plot of {feature}')
+
+                st.plotly_chart(fig, use_container_width=True)
+
+                # Create scatter plot
+                st.subheader("Scatter Plot")
+                x_feature = st.selectbox(label="Select X-Axis Feature", options=num_columns, index=0)
+                y_feature = st.selectbox(label="Select Y-Axis Feature", options=num_columns, index=1)
+
+                scatter_fig = px.scatter(df, x=x_feature, y=y_feature, title=f'Scatter Plot: {x_feature} vs {y_feature}')
+                st.plotly_chart(scatter_fig, use_container_width=True)
+
+            else:
+                st.info("The dataset does not have any numerical columns")
+
+
+            if len(cat_columns)!=0:
+
+                st.subheader("Categorical Variable Analysis")
+                categorical_feature = st.selectbox(label="Select Categorical Feature",options=cat_columns)
+
+                categorical_plot_type = st.selectbox(label="Select Plot Type",options=["Bar Chart","Pie Chart","Stacked Bar Chart","Frequency Count"])
+
+                if categorical_plot_type =="Bar Chart":
+                    fig = px.bar(df,x=categorical_feature,title=f"Bar Chart of {categorical_feature}")
+
+                elif categorical_plot_type == "Pie Chart":
+                    fig = px.pie(df,names=categorical_feature,title=f"Pie Chart of {categorical_feature}")
+
+                elif categorical_plot_type == "Stacked Bar Chart":
+                    st.write("Select a second categorical feature for stacking")
+                    second_categorical_feature = st.selectbox(label="Select Second Categorical Feature",options=cat_columns)
+
+                    fig = px.bar(df,x=categorical_feature,color=second_categorical_feature,title=f"Stacked Bar Chart of {categorical_feature} by {second_categorical_feature}")
+
+                elif categorical_plot_type == "Frequency Count":
+                    cat_value_counts = df[categorical_feature].value_counts()
+                    st.write(f"Frequency Count for {categorical_feature}: ")
+                    st.write(cat_value_counts)
+
+                if categorical_plot_type!= "Frequency Count" and fig is not None:
+                    st.plotly_chart(fig,use_container_width=True)   
+
+
+            else:
+                st.info("The dataset does not have any categorical columns")
+
+
+            st.subheader("Feature Exploration of Numerical Variables")
+            if len(num_columns)!=0:
+                selected_features = st.multiselect("Select Features for Exploration:", num_columns, default=num_columns[:2], key="feature_exploration")
+
+                if len(selected_features) < 2:
+                    st.warning("Please select at least two numerical features for exploration.")
+                else:
+                    st.subheader("Explore Relationships Between Features")
+
+                    # Scatter Plot Matrix
+                    if st.button("Generate Scatter Plot Matrix"):
+                        scatter_matrix_fig = px.scatter_matrix(df, dimensions=selected_features, title="Scatter Plot Matrix")
+                        st.plotly_chart(scatter_matrix_fig, use_container_width=True)
+
+                    # Pair Plot
+                    if st.button("Generate Pair Plot"):
+                        pair_plot_fig = sns.pairplot(df[selected_features])
+                        st.pyplot(pair_plot_fig)
+
+                    # Correlation Heatmap
+                    if st.button("Generate Correlation Heatmap"):
+                        correlation_matrix = df[selected_features].corr()
+                        plt.figure(figsize=(10, 6))
+                        sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
+                        plt.title("Correlation Heatmap")
+                        st.pyplot(plt)     
+
+            else:
+                st.warning("The dataset does not contain any numerical variables")
+
+            # Create a bar graph to get relationship between categorical variable and numerical variable
+            st.subheader("Categorical and Numerical Variable Analysis")
+            if len(num_columns)!=0 and len(cat_columns)!=0:
+                categorical_feature_1 = st.selectbox(label="Categorical Feature", options=cat_columns)
+
+            
+                numerical_feature_1 = st.selectbox(label="Numerical Feature", options=num_columns)
+
+                # Group by the selected categorical column and calculate the mean of the numerical column
+                group_data = df.groupby(categorical_feature_1)[numerical_feature_1].mean().reset_index()
+
+                st.subheader("Relationship between Categorical and Numerical Variables")
+                st.write(f"Mean {numerical_feature_1} by {categorical_feature_1}")
+                
+                # Create a bar chart
+                fig = px.bar(group_data, x=categorical_feature_1, y=numerical_feature_1, title=f"{numerical_feature_1} by {categorical_feature_1}")
+                st.plotly_chart(fig, use_container_width=True)
+
+            else:
+                st.warning("The dataset does not have any numerical variables. Hence Cannot Perform Categorical and Numerical Variable Analysis")
+            
+
+    if data_preprocessing_button:
+        st.header("🛠️ Data Preprocessing(To be implemented)")
